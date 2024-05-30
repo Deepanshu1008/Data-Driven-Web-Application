c9e283bc4c4be5277fbf51cbab91b9919c45dadc
Deepanshu Gupta <89475845+Deepanshu1008@users.noreply.github.com>
2024-05-31 03:14:01 +0530

    Delete Data-Driven Web Application/csv-upload-app/client directory

--- Data-Driven Web Application/csv-upload-app/client/examples/src/index.js
+++
@@ -1,8 +0,0 @@
-// examples/src/index.js
-import React from "react";
-import ReactDOM from "react-dom";
-import CsvInterface from "../../src/CsvInterface";  // Adjust the import path as needed
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<CsvInterface />, rootElement);
-
