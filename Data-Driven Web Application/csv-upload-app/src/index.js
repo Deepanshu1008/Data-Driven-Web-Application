fb6d6e41c07e4478d83b8350f2ef9aaa6f234440
Deepanshu Gupta <89475845+Deepanshu1008@users.noreply.github.com>
2024-05-31 03:22:56 +0530

    Update index.js

--- Data-Driven Web Application/csv-upload-app/src/index.js
+++ Data-Driven Web Application/csv-upload-app/src/index.js
@@ -1,8 +1,7 @@
-// examples/src/index.js
+
 import React from "react";
 import ReactDOM from "react-dom";
 import CsvInterface from "../../src/CsvInterface";  // Adjust the import path as needed
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<CsvInterface />, rootElement);
-
